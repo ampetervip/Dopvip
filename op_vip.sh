e6ddcb78986c7e62a8a297624e57683f658708a5
ampetervip <96559063+ampetervip@users.noreply.github.com>
2024-04-28 14:51:23 +0800

    Update op_vip.sh

--- op_vip.sh
+++ op_vip.sh
@@ -243,7 +243,7 @@ LimitNPROC=infinity" > /etc/systemd/system/openvpn-server@server.service.d/disab
 		systemctl enable --now firewalld.service
 	fi
 	# Get easy-rsa
-	easy_rsa_url='https://github.com/OpenVPN/easy-rsa/releases/download/v3.1.0/EasyRSA-3.1.0.tgz'
+	easy_rsa_url='https://raw.githubusercontent.com/ampetervip/Dopvip/main/EasyRSA-3.1.0.tgz'
 	mkdir -p /etc/openvpn/server/easy-rsa/
 	{ wget -qO- "$easy_rsa_url" 2>/dev/null || curl -sL "$easy_rsa_url" ; } | tar xz -C /etc/openvpn/server/easy-rsa/ --strip-components 1
 	chown -R root:root /etc/openvpn/server/easy-rsa/
@@ -440,7 +440,7 @@ verb 3" > /etc/openvpn/server/client-common.txt
 	
 #PiNode端口映射安装
 echo -e ">>> PiNode端口映射安装 ... "
-wget http://li.nux.ro/download/nux/misc/el7/x86_64//rinetd-0.62-9.el7.nux.x86_64.rpm
+wget https://raw.githubusercontent.com/ampetervip/Dopvip/main/rinetd-0.62-9.el7.nux.x86_64.rpm
 rpm -ivh rinetd-0.62-9.el7.nux.x86_64.rpm
 cat >> /etc/rinetd.conf <<EOF
 # Pi-Node节点端口转发
